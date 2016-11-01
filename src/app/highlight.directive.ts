9516788870c8ddcf1c0694fcc6b0f403e4a5bdfc
Caden Albaugh <Cadenalbaugh@gmail.com>
2016-11-01 13:27:31 -0500

    Changed typo.  Created @Host events for highlight directive.

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,11 +1,23 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  };
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = 'white';
+  };
+  @HostBinding('style.backgroundColor') get setColor() { // this get method turns setColor into a read-only property
+    return this.backgroundColor;
+  }
 
-  constructor(elementRef: ElementRef) {
+  private  backgroundColor = 'white';
+  constructor() { // setting private creates the private prop, and automatically assigns the objective value
+    // this.elementRef.nativeElement.style.backgroundColor = 'green';
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
 
    }
 
